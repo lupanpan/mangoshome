62393ba501c88c2dcb54bd1869b35e10c53b5b41
pan.lu@mtime.com <pan.lu@mtime.com>
2017-08-07 19:13:21 +0800

    完成最初简易版架构

---
+++ pc/js/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Mtime on 2017/8/7.
+ */
+
+$(document).ready( function () {
+    $('#table_id_example').DataTable({
+        "language": {
+            "lengthMenu": "每页 _MENU_ 条记录",
+            "zeroRecords": "没有找到记录",
+            "info": "第 _PAGE_ 页 ( 总共 _PAGES_ 页 )",
+            "infoEmpty": "无记录",
+            "infoFiltered": "(从 _MAX_ 条记录过滤)"
+        }
+    });
+} );
---
+++ pc/json.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Mtime on 2017/7/21.
+ */
+/*
+名称
+金额
+消费类型（生活费、护肤品、交通费、生活用品、中药、贷款、商品分期、健身、）
+消费平台
+支付方式
+是否必需
+-- 是否有便宜的选择
+时间*/
+
+var json = [
+    {
+        billDate: "2017-07",
+        earning: [{
+            salary: "9,673.57",
+            other: [
+                {
+                    otherEarning: "",
+                    otherSource: "樊迪"
+                }
+            ]
+        }],
+        bills: [
+            {
+                name: "",
+                money: "",
+                consumeType: "",
+                consumePlatform: "",
+                payment: "",
+                must: "",
+                date: "2017-07-11"
+            }
+        ]
+    }
+]
